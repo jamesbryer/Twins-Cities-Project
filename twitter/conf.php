495020be5e7fe02492b3d5ef6e364f1d9ae0df52
jamesbryer <36200875+jamesbryer@users.noreply.github.com>
2023-02-08 23:19:10 +0000

    remove API keys

--- twitter/conf.php
+++ twitter/conf.php
@@ -3,26 +3,26 @@
     #API keys 
 
     #Twitter
-    define ("TWITTER_CONSUMER_KEY", "tmt3K6qh0tIQBM4u2e0v7Wlt9");
-    define ("TWITTER_CONSUMER_SECRET", "GzkWQheXYohvPapxIkvSnBx8j07khvrSTwmdj5QgAzwaeXvyj5");
-    define ("TWITTER_ACCESS_TOKEN", "1464969227526410241-DPDLIM4XaAMs1vJA7GSF4sIMJAzhOf");
-    define ("TWITTER_ACCESS_TOKEN_SECRET", "Kx7zf7mZNm9klSNFc7TVMqF43p81E0DrxBeXxYXr7WcDr");
-    define ("TWT_API_BEARER_TKN", "AAAAAAAAAAAAAAAAAAAAALUbWQEAAAAAXYF4PHnsiEGlBgXgh0F%2BtWWIpBI%3DTzvqanJS5RvD2ZQRLiZzikYTJR5ieOp9l4URPMXfrMzVRhXUUp");
+    define ("TWITTER_CONSUMER_KEY", "");
+    define ("TWITTER_CONSUMER_SECRET", "");
+    define ("TWITTER_ACCESS_TOKEN", "");
+    define ("TWITTER_ACCESS_TOKEN_SECRET", "");
+    define ("TWT_API_BEARER_TKN", "");
     define ("SEARCH_API_URL_1", "https://api.twitter.com/2/tweets/search/recent?query=");
     define ("SEARCH_API_URL_2", "(-is:retweet)(-is:quote)(-is:reply)(lang:en)&tweet.fields=created_at&expansions=author_id&user.fields=created_at");
 
     #Google Maps
 
-    define ("GGLE_MAP_API_KEY", "AIzaSyCQAPN2VQNLL29iwx1gbgR9dDJCf_Hbd_k");
+    define ("GGLE_MAP_API_KEY", "");
 
     #OpenWeatherData
 
-    define ("OPN_WEATHER_API_KEY", "565e980f6212df1ad19dfe535174a56f&");
+    define ("OPN_WEATHER_API_KEY", "");
 
     #Flickr
 
-    define ("FLICKR_API_KEY" , "5bc3100f326b85d41a9168803478509d");
-    define ("FLICKR_API_SECRET", "20f35db4576c60d5");
+    define ("FLICKR_API_KEY" , "");
+    define ("FLICKR_API_SECRET", "");
 
     #MySQL Database Connection Info
 
@@ -32,10 +32,4 @@
     define("PASSWORD","1234");
     define("DBNAME","twin_cities")
     
-
-
-
-
-
-
 ?>
